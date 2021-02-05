b1b57cd96f8203d603d0dc43ffac53b258d19b41
alisa-p <alisa.online@web.de>
2021-02-05 09:33:28 +0200

    updated init file to include plotting

--- graphAlgorithms/__init__.py
+++ graphAlgorithms/__init__.py
@@ -3,5 +3,5 @@ from .communities import *
 from .pattern_matching import *
 from .simplification import *
 from graphAlgorithms.distances import *
-
+from .plotting import *
 from graphAlgorithms.example_pipeline_wrappers import *
