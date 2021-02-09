75d37eb1e60945e15fb9c276decd51777a4dc8c5
alisa-p <alisa.online@web.de>
2021-02-09 11:00:32 +0200

    added agreement matrix plotting

--- graphAlgorithms/plotting.py
+++ graphAlgorithms/plotting.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 from collections import Counter
 import matplotlib.patches
 import numpy as np
+from scipy.stats.stats import pearsonr 
 
-
-def plot_heatmap(matrix, xlabels=None, ylabels=None, size=(10,8), cmap="bone"):
+def plot_heatmap(matrix, xlabels=None, ylabels=None, size=(10,8), cmap="bone", annotation=False):
 
     """
     Plots a heatmap. Return figure fig can be saved with fig.savefig(). For parameters refer to the matplotlib documentation.
@@ -21,6 +21,7 @@ def plot_heatmap(matrix, xlabels=None, ylabels=None, size=(10,8), cmap="bone"):
         ylabels (list or None): list of tick labels to be used for the y axis
         size (tuple): plot size to be used
         cmap (matplotlib colormap): can be created colormap or name of a pre-defined color map
+        annotation (boolea): if True then cell values are plotted
 
     Returns:
         fig (matplotlib figure): heatmap object
@@ -36,7 +37,7 @@ def plot_heatmap(matrix, xlabels=None, ylabels=None, size=(10,8), cmap="bone"):
 
     fig, ax = plt.subplots(figsize=size)  
 
-    sns.heatmap(matrix, annot=False, ax=ax, xticklabels=xlabels, yticklabels=ylabels, cmap=cmap)
+    sns.heatmap(matrix, annot=annotation, ax=ax, xticklabels=xlabels, yticklabels=ylabels, cmap=cmap)
 
     return fig
 
@@ -164,3 +165,74 @@ def plot_clustering_heatmap(clusters, matrix, labels, cmap="bone", size=(10,8)):
     fig.colorbar(coll)
 
     return fig
+
+
+def plot_agreement_matrix(clusterings, xlabels=None, ylabels=None, size=(10,8), cmap="bone", annotation=True):
+    """
+    Calculates the agreement matrix between provided clusterings, which is plotted as a heatmap. For each item pair a value is estimated how often
+    this item is grouped together between the provided algorithms.
+
+    Parameters:
+        clusterings (list): list of clusterings. Clusterings need to be lists and items need to be in same order between the provided
+            clusterings.
+        xlabels (list or None): list of tick labels to be used for the x axis
+        ylabels (list or None): list of tick labels to be used for the y axis
+        size (tuple): plot size to be used
+        cmap (matplotlib colormap): can be created colormap or name of a pre-defined color map
+        annotation (boolea): if True then cell values are plotted
+
+    Returns:
+        fig (matplotlib figure): heatmap object
+        aggreement matrix (matrix): containing the agreement values
+    """
+
+    agg =  np.zeros((len(clusterings[0]), len(clusterings[0])))
+
+    for index, x in np.ndenumerate(agg):
+        cnt = 0
+        for c in clusterings:
+            if c[index[0]] == c[index[1]]:
+                cnt = cnt + 1
+                
+        agg[index[0]][index[1]] = cnt
+
+
+
+    f = plot_heatmap(agg, xlabels=xlabels, ylabels = ylabels, size=size, cmap=cmap, annotation=annotation)
+
+
+    return f, agg
+
+
+
+
+def plot_correlation_clusterings(clusterings, xlabels=None, ylabels=None, size=(10,8), cmap="bone"):
+    """
+    Calculates the pearson correlation between pairs of cluster vectors and plots a cluster map.
+
+    Parameters:
+        clusterings (list): list of clusterings. Clusterings need to be lists and items need to be in same order between the provided
+            clusterings. Individual items need to be int.
+        xlabels (list or None): list of tick labels to be used for the x axis
+        ylabels (list or None): list of tick labels to be used for the y axis
+        size (tuple): plot size to be used
+        cmap (matplotlib colormap): can be created colormap or name of a pre-defined color map
+        
+
+    Returns:
+        fig (matplotlib figure): heatmap object
+        correaltion matrix (matrix): containing the correaltion values
+    """
+
+    cor =  np.zeros((len(clusterings), len(clusterings)))
+
+    for index, x in np.ndenumerate(cor):
+        c, p = pearsonr(clusterings[index[0]], clusterings[index[1]])
+        cor[index[0]][index[1]] = c
+
+
+
+    f = plot_hierarchical_clustering(cor, xlabels=xlabels, ylabels=ylabels, size=size, cmap=cmap)
+
+
+    return f, cor
